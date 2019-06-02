27d7c91d8229f3a2f4672596ac3b0d1fdda51baf
lowch <lowchenghong@yahoo.com.sg>
2019-06-02 22:04:07 +0800

    Add project files.

---
+++ HelloWorldService/HelloWorldService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWCFServices
+{
+    public class HelloWorldService: IHelloWorldService
+    {
+        public string GetMessage(string name)
+        {
+            return "Hello world from " + name + "!";
+        }
+    }
+}
